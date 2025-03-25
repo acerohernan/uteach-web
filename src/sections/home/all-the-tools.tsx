aa1c82aeac494ef9658d380746564597100392d6
Hernan Acero <contacto.acero.hernan@gmail.com>
2025-03-25 12:43:15 -0500

    feat: all the tools dektop images added

--- src/sections/home/all-the-tools.tsx
+++ src/sections/home/all-the-tools.tsx
@@ -23,13 +23,50 @@ export const HomeAllTheTools = (): React.ReactElement => {
           </p>
         </div>
         {/* Image and background containter */}
-        <div className="relative lg:hidden">
-          <img className="w-full p-4 z-10 relative" src="/images/uteach-page-2.svg" alt="title line" />
-          <img className="-mt-4 w-[19%] ml-[34.6%]" src="/images/tools-line.svg" alt="title line" />
-          <img className="w-full pl-4" src="/images/uteach-tools.svg" alt="title line" />
-          {/* Background */}
-          <img className="w-[30%] absolute top-0 left-0" src="/images/bg-tools-1.svg" alt="title line" />
-          <img className="w-[32.56%] absolute right-0 top-[-47%]" src="/images/bg-tools-2.svg" alt="title line" />
+        <div className="relative lg:mt-20">
+          <img
+            className="w-full p-4 z-10 relative lg:hidden"
+            src="/images/uteach-page.svg"
+            alt="uteach page ilustration"
+          />
+          <img
+            className="hidden lg:block w-full p-4 z-10 relative"
+            src="/images/uteach-page-desktop.svg"
+            alt="uteach page ilustration"
+          />
+
+          <img className="-mt-4 w-[19%] ml-[34.6%] lg:hidden" src="/images/tools-line.svg" alt="title line" />
+          <img
+            className="-mt-15 w-[90%] hidden lg:block lg:mx-auto"
+            src="/images/tools-line-desktop.svg"
+            alt="title line"
+          />
+
+          <img className="w-full pl-4 lg:hidden" src="/images/uteach-tools.svg" alt="uteach tools ilustration" />
+          <img
+            className="w-full pl-4 hidden lg:block"
+            src="/images/uteach-tools-desktop.svg"
+            alt="uteach tools ilustration"
+          />
+          {/* Mobile background */}
+          <img className="w-[30%] absolute top-0 left-0 lg:hidden" src="/images/bg-tools-1.svg" alt="title line" />
+          <img
+            className="w-[32.56%] absolute right-0 top-[-47%] lg:hidden"
+            src="/images/bg-tools-2.svg"
+            alt="title line"
+          />
+
+          {/* Desktop background */}
+          <img
+            className="hidden lg:block w-[43.5%] absolute top-0 -ml-32"
+            src="/images/bg-tools-1-desktop.svg"
+            alt="title line"
+          />
+          <img
+            className="hidden lg:block w-[40.5%] absolute top-[-320px] right-[-50px]"
+            src="/images/bg-tools-2-desktop.svg"
+            alt="title line"
+          />
         </div>
       </div>
     </div>
